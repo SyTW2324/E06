55c0a750b6a3b869188302ade292dd1be85a214e
Ivan Garcia Glez <alu010138786@ull.edu.es>
2023-11-13 18:12:46 +0000

    trying to start the vue client

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,29 +1,8 @@
-import * as dotenv from "dotenv";
-import cors from "cors";
 import express from "express";
-import { connectToDatabase } from "./database";
-import { employeeRouter } from "./employee.routes";
 
-// Load environment variables from the .env file, where the BDD_SYTW_URI is configured
-dotenv.config();
+const app = express();
 
-const { BDD_SYTW_URI } = process.env;
-
-if (!BDD_SYTW_URI) {
-    console.error("No BDD_SYTW_URI environment variable has been defined in config.env");
-    process.exit(1);
-}
-
-connectToDatabase(BDD_SYTW_URI)
-    .then(() => {
-        const app = express();
-        app.use(cors());
-        app.use("/employees", employeeRouter);
-
-        // start the Express server
-        app.listen(5200, '172.16.30.2', () => {
-	    console.log('Server running at http://172.16.30.2:5200...');
-        });
-
-    })
-    .catch(error => console.error(error));
+// start the Express server
+app.listen(3000, () => {
+  console.log('Server running at http://localhost:3000...');
+});
