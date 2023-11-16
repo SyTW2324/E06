cac470805ba1087567997044e9a2c2f83feb8de2
Daniel Jorge Acosta <alu0101239187@ull.edu.es>
2023-11-16 19:33:08 +0000

    Desarrollada página principal

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,18 +4,21 @@
 
 <template>
     <div class="navbar"><Navbar /></div>
-    
 
     <div class="main-screen">
       <div class="main-screen-text">
         <h1>TU</h1>
         <h2>página de pesca</h2>
-        <h2><a>Regístrate</a></h2>
+        <br/><br/><br/><br/><br/><br/>
+        <button type="button" class="btn btn-primary">Regístrate</button>
       </div>
     </div>
 </template>
 
 <style scoped>
+#app {
+  left: 50%;
+}
 .navbar {
   position: fixed;
   top: 0;
@@ -24,25 +27,41 @@
   z-index: 1000;
 }
 .main-screen {
-  width: 140%;
-  color: white;
+  width: 34%;
   background-color: #455a64e0;
-  border-radius: 25px;
+  border-radius: 2rem;
   overflow: hidden;
-  padding: 20% 20%;
+  padding-top: 2%;
+  padding-bottom: 2%;
+  top: 30%;
+  left: 33%;
+  position: absolute;
 }
 .main-screen-text {
+  color: white;
   width: 100%;
 }
 
 .main-screen-text h1 {
-  font-size: 300%;
+  font-size: 4rem;
   font-weight: 500;
   text-align: center;
 }
 
+.main-screen-text .btn {
+  width: 40%;
+  font-size: 1.5rem;
+  background-color: #00A0D1;
+  border-color: #00A0D1;
+  border-radius: 3rem;
+  overflow: hidden;
+  padding: 2%;
+  left: 30%;
+  position: relative;
+}
+
 .main-screen-text h2 {
-  font-size: 200%;
+  font-size: 300%;
   font-weight: 500;
   text-align: center;
 }
--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -3,48 +3,70 @@
 
 <!-- Navbar.vue -->
 <template>
+  <div class ="navBar">
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img src="../../images/iso.png" alt="" width="40" height="32">FISHING NET
+        </a>
 
-<div>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto pe-3">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="#">Foro</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="#">Fauna</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="#">Mapa</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="#">Marea</a>
+            </li>
           </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
+          <a class="nav-item" href="#">
+            <img src="../../images/user_icon.png" alt="" width="40px" height="40px">
+          </a>
+        </div>
+      </div>
+    </nav>
   </div>
-</nav>
-</div>
 </template>
 
 
 <style>
+.navBar{
+  width: 100%;
+  background-color: #00A0D1;
+  position: absolute;
+  top: 0;
+  padding-left: 1%;
+  padding-right: 1%;
+}
 
+.navbar-brand img { 
+  height: 8%;
+  width: 8%;
+  margin-right: 2%;
+}
+.navbar-brand { 
+  color: white; 
+  font-size: 2rem;
+  align-items: center;
+}
+
+.navbar-nav .nav-link.active { /* Cuando esta activo*/
+  color: white; 
+  font-size: 20px; 
+  font-weight: bold;
+}
+
+.navbar-nav .nav-link {
+  color: white; 
+  font-size: 20px; 
+}
 </style>
