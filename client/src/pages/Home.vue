986ecc13de238eca22560e0578eb2e957f6f914f
Daniel Jorge Acosta <alu0101239187@ull.edu.es>
2024-01-16 11:27:33 +0000

    cambio home link

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -18,7 +18,10 @@ export default {
       <h1>TU</h1>
       <h2>página de pesca</h2>
       <div class="spacer"></div>
-      <a v-if="!userLogged" type="button" class="btn btn-primary" href="register">Regístrate</a>
+      <!--<a v-if="!userLogged" type="button" class="btn btn-primary" href="register">Regístrate</a>-->
+      <router-link v-if="!userLogged" to="/register" tag="button" class="btn btn-primary"
+        >Regístrate</router-link
+      >
     </div>
   </div>
 </template>
