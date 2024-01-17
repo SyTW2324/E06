ae2f166a9baa66f8640ce78f826f57b4c257dd35
Ivan <ivitacoronte@hotmail.com>
2024-01-17 14:15:57 +0000

    minor changes

--- client/cypress/e2e/fishes.cy.js
+++ client/cypress/e2e/fishes.cy.js
@@ -22,7 +22,7 @@ describe("Pagina de Fauna", () => {
 
   it("Debería buscar el sargo", () => {
     cy.get('input[placeholder="Buscar por nombre..."]').type("sargo");
-    cy.get('.btn.btn-secondary').click();
+    cy.contains('button', 'Buscar').click();
 
     // Deberian aparecer al menos el Sargo comun 
     cy.contains('Sargo común').should('be.visible');
